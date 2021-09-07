f048dcfeca92dda1c4f27918c2593c783430fe53
xbamsod <mimidalena@gmail.com>
2021-09-07 16:31:49 -0700

    add product feature

--- src/main/java/com/example/myapplication/MyApplication.java
+++ src/main/java/com/example/myapplication/MyApplication.java
@@ -15,6 +15,7 @@ import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.FirestoreOptions;
 import com.google.cloud.firestore.Query;
@@ -22,13 +23,12 @@ import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
 import com.google.cloud.firestore.WriteResult;
 import com.google.common.collect.ImmutableMap;
-import com.sendgrid.*;
-import com.sendgrid.Method;
-import com.sendgrid.Request;
-import com.sendgrid.SendGrid;
-import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Content;
-import com.sendgrid.helpers.mail.objects.Email;
+// import com.sendgrid.Method;
+// import com.sendgrid.Request;
+// import com.sendgrid.SendGrid;
+// import com.sendgrid.helpers.mail.Mail;
+// import com.sendgrid.helpers.mail.objects.Content;
+// import com.sendgrid.helpers.mail.objects.Email;
 
 
 import org.springframework.boot.SpringApplication;
@@ -46,11 +46,6 @@ public class MyApplication {
         SpringApplication.run(MyApplication.class, args);
     }
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello world!";
-    }
-
     @PostMapping("/notify-me")
     public String notifyMe(@RequestBody Map<String, String> body)
             throws IOException, InterruptedException, ExecutionException {
@@ -64,26 +59,60 @@ public class MyApplication {
         return new ObjectMapper().writeValueAsString(body);
     }
 
-    private void sendEmail(String email) throws IOException {
-        Email from = new Email("mimidalena@gmail.com");
-        String subject = "Full Stack Cloud Developer Course Registration";
-        Email to = new Email(email);
-        Content content = new Content("text/plain", "You've been added to the list! We'll notify you when registration begins.");
-        Mail mail = new Mail(from, subject, to, content);
+    // private void sendEmail(String email) throws IOException {
+    //     Email from = new Email("mimidalena@gmail.com");
+    //     String subject = "Full Stack Cloud Developer Course Registration";
+    //     Email to = new Email(email);
+    //     Content content = new Content("text/plain", "You've been added to the list! We'll notify you when registration begins.");
+    //     Mail mail = new Mail(from, subject, to, content);
 
-        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
-        Request request = new Request();
-        try {
-            request.setMethod(Method.POST);
-            request.setEndpoint("mail/send");
-            request.setBody(mail.build());
-            Response response = sg.api(request);
-            System.out.println(response.getStatusCode());
-            System.out.println(response.getBody());
-            System.out.println(response.getHeaders());
-        } catch (IOException ex) {
-            throw ex;
+    //     SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+    //     Request request = new Request();
+    //     try {
+    //         request.setMethod(Method.POST);
+    //         request.setEndpoint("mail/send");
+    //         request.setBody(mail.build());
+    //         Response response = sg.api(request);
+    //         System.out.println(response.getStatusCode());
+    //         System.out.println(response.getBody());
+    //         System.out.println(response.getHeaders());
+    //     } catch (IOException ex) {
+    //         throw ex;
+    //     }
+    // }
+
+    @GetMapping("/email-list")
+    public List<Map<String, Object>> getEmails() throws IOException, InterruptedException, ExecutionException{
+        List<Map<String, Object>> list = new ArrayList<>();
+        Firestore db = getFirestore();
+        ApiFuture<QuerySnapshot> query = db.collection("emails").get();
+        QuerySnapshot querySnapshot = query.get();
+        List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
+
+        for (QueryDocumentSnapshot document : documents) {
+            list.add(document.getData());
         }
+        return list;
+    }
+
+    @PostMapping("/product-post")
+    public String productList(@RequestBody Map<String, Object> body)
+        throws IOException, InterruptedException, ExecutionException {
+        Firestore db = getFirestore();
+
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("ProductID", body.get("id"));
+        data.put("Name", body.get("name"));
+        data.put("Price", body.get("price"));
+        data.put("Stock", body.get("stock"));
+
+        DocumentReference document = db.collection("products").document((String) body.get("id"));
+
+        ApiFuture<WriteResult> result = document.set(data);
+
+        System.out.println("Update time : " + result.get().getUpdateTime());
+        return new ObjectMapper().writeValueAsString(body);
     }
 
     private Firestore getFirestore() throws IOException {
--- src/main/java/com/example/myapplication/WebConfiguration.java
+++ src/main/java/com/example/myapplication/WebConfiguration.java
@@ -1,40 +1,40 @@
-package com.example.myapplication;
+// package com.example.myapplication;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-
-@Configuration
-public class WebConfiguration extends WebSecurityConfigurerAdapter{
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-			.antMatchers("/app/**").authenticated()
-			.antMatchers("/**").permitAll()
-			.and()
-			.oauth2Login() //oauth
-			.and()
-			.logout()
-				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-				.logoutSuccessUrl("/");
-	}
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+// import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
-	/**
-	 * This handles the form username and password auth.
-	 *
-	 * Username: admin
-	 * Password: password
-	 */
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth .inMemoryAuthentication()
-			.withUser("admin")
-			.password("{noop}password")
-			.roles("ADMIN");
-	}
-}
+// @Configuration
+// public class WebConfiguration extends WebSecurityConfigurerAdapter{
+// 	@Override
+// 	protected void configure(HttpSecurity http) throws Exception {
+// 		http
+// 			.authorizeRequests()
+// 			.antMatchers("/app/**").authenticated()
+// 			.antMatchers("/**").permitAll()
+// 			.and()
+// 			.oauth2Login() //oauth
+// 			.and()
+// 			.logout()
+// 				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+// 				.logoutSuccessUrl("/");
+// 	}
+
+
+// 	/**
+// 	 * This handles the form username and password auth.
+// 	 *
+// 	 * Username: admin
+// 	 * Password: password
+// 	 */
+// 	@Override
+// 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+// 		auth .inMemoryAuthentication()
+// 			.withUser("admin")
+// 			.password("{noop}password")
+// 			.roles("ADMIN");
+// 	}
+// }
